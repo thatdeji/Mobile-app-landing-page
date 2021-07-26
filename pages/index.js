683edafa02892eda026d942a0aedbb153e098969
Akindeji <akindejioluwaseun015@gmail.com>
2021-07-26 05:57:26 -0700

    completed qr button animation

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,17 @@ const textVariants = {
   initial: { y: "70px" },
   animate: { y: 0, transition: { duration: 0.8, delay: 0.8 } }
 };
-const scannerVariants = {
-  initial: { y: 0, x: "-50%" },
-  animate: { y: "-48px", transition: { duration: 0.8, delay: 1 } }
+const boxVariants = {
+  initial: { backgroundColor: "#2ac940", scale: 1 },
+  animate: {
+    backgroundColor: "#fff",
+    scale: 1.6,
+    transition: { duration: 0.8, delay: 1 }
+  }
 };
-const qrVariants = {
-  initial: { y: "49px", x: "-50%" },
-  animate: { y: "7.5px", transition: { duration: 0.8, delay: 1 } }
+const boxAnimVariants = {
+  initial: { y: 0, x: "-50%" },
+  animate: { y: "-54px", transition: { duration: 0.8, delay: 1 } }
 };
 
 export default function Home() {
@@ -81,22 +85,22 @@ export default function Home() {
           </h1>
           <div className={styles.codebtn}>
             {/*<Image width="64" height="64" src="/Code Button.png" />*/}
-            <div className={styles.box}>
-              <motion.img
-                variants={scannerVariants}
-                className={styles.scanner}
-                width="48"
-                height="48"
-                src="/Scanner 1.svg"
-              />
-              <motion.img
-                variants={qrVariants}
-                className={styles.qr}
-                width="54"
-                height="49"
-                src="/QR code 1.svg"
-              />
-            </div>
+            <motion.div variants={boxVariants} className={styles.box}>
+              <motion.div className={styles.boxAnim} variants={boxAnimVariants}>
+                <motion.img
+                  className={styles.scanner}
+                  width="48"
+                  height="48"
+                  src="/Scanner 1.svg"
+                />
+                <motion.img
+                  className={styles.qr}
+                  width="54"
+                  height="49"
+                  src="/QR code 1.svg"
+                />
+              </motion.div>
+            </motion.div>
           </div>
         </section>
         <section className={styles.showcase}>
